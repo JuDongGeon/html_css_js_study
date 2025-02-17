9f6117ca92183979c9f788e77e9128ab08d3615c
JuDongGeon <ehdrjs981024@gmail.com>
2025-02-17 17:27:54 +0900

    가위바위보 복습 중

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const imgs = [
   "https://cdn-icons-png.flaticon.com/128/3562/3562093.png",
   "https://cdn-icons-png.flaticon.com/128/12355/12355903.png",
 ];
+
+const root = document.querySelector("#root");
+const con = document.querySelector(".con");
+const h1 = document.querySelector("h1");
+const button = document.querySelector("button");
+const images = document.querySelectorAll("img");
+
+imges.forEach((image, index) => image.sr);
